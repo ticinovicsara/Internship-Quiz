3979d30eccbbd25abdde12ece11a2525fdf2bd07
ticinovicsara <ticinovicsara0@gmail.com>
2025-04-18 22:30:23 +0200

    add apps folder and turbo.json file

--- apps/frontend/src/components/ProtectedRoute.tsx
+++ apps/frontend/src/components/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { isAuthenticated } from "../utils/auth";
 
 const ProtectedRoute = () => {
-  return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
+  return isAuthenticated() ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
